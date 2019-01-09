75473832da9ea3690d86c58ea903c4246c1436e4
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-08 16:27:46 -1000

    removed extra paste

--- js/app.js
+++ js/app.js
@@ -12,44 +12,7 @@ function makeList(n) {
 
 // Quick sort
 function myQuickAlgorithm() {
-  (function myMergeAlgorithm() {
-  const list = makeList(10);
-  function mergeSort(array) {
-    if (array.length === 1) {
-      return array;
-    }
-
-    const middle = Math.floor(array.length / 2);
-    const left = array.slice(0, middle);
-    const right = array.slice(middle); // from the middle on
-
-    return merge(
-      mergeSort(left),
-      mergeSort(right),
-    );
-  }
-
-  function merge(left, right) {
-    let result = [ ];
-    let indexLeft = 0;
-    let indexRight = 0;
-
-    while (indexLeft < left.length && indexRight < right.length) {
-      if (left[indexLeft] < right[indexRight]) {
-        result.push(left[indexLeft]);
-        indexLeft++;
-      } else {
-        result.push(right[indexRight]);
-        indexRight++;
-      }
-    }
-
-    return result.concat(left.slice(indexLeft), right.slice(indexRight));
-  }
-
-  console.log(`Merge sort:`);
-  console.log(mergeSort(list)); // [1, 2, 2, 3, 3, 3, 5, 6, 7]
-})();
+  
 }
 
 /*  Merge sort...
